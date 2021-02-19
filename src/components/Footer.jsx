fb760001f49e1241b3f21290e156670cd171a23f
Jason Quizhpi <jasonquizhpi@Jasons-MacBook-Pro-2.local>
2021-02-18 20:47:03 -0500

    updated css styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ function Footer() {
   return (
     <div className= "Footer">
       <h5>© Jason Quizhpi 2021 </h5>
-      <div  class="icon "><a className="link" href ="https://github.com/jquizhpi96"><img className="avatar"src=" https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png" alt="LinkedIn"></img></a></div>
-        <div  class="icon"><a className="link" href="https://www.linkedin.com/in/jason-quizhpi-30a41b169/"><img className="avatar" src=" https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" alt="Github"></img></a></div>
+      <div  className="icon "><a className="link" href ="https://github.com/jquizhpi96"><img className="avatar"src=" https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png" alt="LinkedIn"></img></a></div>
+        <div  className="icon"><a className="link" href="https://www.linkedin.com/in/jason-quizhpi-30a41b169/"><img className="avatar" src=" https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" alt="Github"></img></a></div>
         
     </div>
   )
